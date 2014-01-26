8aead8be603d41d923f40ad2f30e70d56abcfebd
Amol Phalnikar <amol.phalnikar@atos.net>
2014-01-26 23:00:28 +0000

    Test Class 1.1
    Test class in 1.1

---
+++ src/test/java/com/test/pkg1/TestServiceTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.test.pkg1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author a510774
+ *
+ */
+public class TestServiceTest {
+
+	/**
+	 * Test method for {@link com.test.pkg1.TestServiceImpl#createAccount(java.lang.Integer)}.
+	 */
+	@Test
+	public final void testCreateAccount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
